e111f8779f34e51e520e5d8fa0347545601d2ff5
watcher041 <watcher041@gmail.com>
2025-03-22 09:53:41 +0900

    追記

---
+++ SQL/SQL_Server/04_case.sql
@@ -0,0 +1,19 @@
+
+-- 第4回 CASE文
+
+-- テーブルからの値を元に別の値を表示するときに利用
+-- 「COLUMN1」が0なら「馬」になり、「COLUMN2」が1なら「鹿」になる。
+-- それ以外なら「無」になる。
+
+SELECT
+    (CASE
+        WHEN COLUMN1 = 0 THEN '馬'
+        WHEN COLUMN1 = 1 THEN '鹿'
+        ELSE '無'
+    END) AS CASE_RESULT,
+    COLUMN2
+FROM
+(
+    SELECT 
+        1 AS COLUMN1
+) AS SUB_QUERY_T
